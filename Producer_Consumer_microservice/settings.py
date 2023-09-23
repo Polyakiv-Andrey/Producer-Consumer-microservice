4a813f58ba17714e4c751d9f9dec9887c58ca6b8
andrey <polyakovv.andrey.01@gmail.com>
2023-09-23 12:19:21 +0300

    added celery to project

--- Producer_Consumer_microservice/settings.py
+++ Producer_Consumer_microservice/settings.py
@@ -128,3 +128,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'order.Employee'
+
+
+# Celery
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERYD_HIJACK_ROOT_LOGGER = False
